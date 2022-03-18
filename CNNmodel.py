6a092c5358320132a6a3d96fc4cf6d0190d4b9a1
Subhadharshini <69450609+Subhadharshini@users.noreply.github.com>
2022-03-18 20:49:38 +0530

    Add files via upload

---
+++ CNNmodel.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 05 10:26:01 2020
+
+@author: Subi K
+"""
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense, Dropout
+from keras import optimizers
+classifier = Sequential() #CNN Initializing
+classifier.add(Convolution2D(32, 3,  3, input_shape = (64, 64, 3), activation = 'relu')) #Conv Layer
+classifier.add(MaxPooling2D(pool_size =(2,2))) #Max pooling 
+classifier.add(Convolution2D(32, 3,  3, activation = 'relu')) #2nd Conv Layer
+classifier.add(MaxPooling2D(pool_size =(2,2)))
+classifier.add(Convolution2D(64, 3,  3, activation = 'relu')) #3rd Conv Layer
+classifier.add(MaxPooling2D(pool_size =(2,2)))
+classifier.add(Flatten()) #Flatten
+classifier.add(Dense(256, activation = 'relu')) #Fully connected layers
+classifier.add(Dropout(0.5))
+classifier.add(Dense(26, activation = 'softmax'))
+
+classifier.compile(
+              optimizer = optimizers.SGD(lr = 0.01),
+              loss = 'categorical_crossentropy',
+              metrics = ['accuracy'])
+
+from keras.preprocessing.image import ImageDataGenerator
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+test_datagen = ImageDataGenerator(rescale=1./255)
+training_set = train_datagen.flow_from_directory(
+        'Dataset/training_set',
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='categorical')
+test_set = test_datagen.flow_from_directory(
+        'Dataset/test_set',
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='categorical')
+model = classifier.fit_generator(
+        training_set,
+        steps_per_epoch=800,
+        epochs=25,
+        validation_data = test_set,
+        validation_steps = 6500
+      )
+
+import h5py
+classifier.save('Trainedmodel.h5')
+print(model.history.keys())
+import matplotlib.pyplot as plt
+# summarize history for accuracy
+plt.plot(model.history['acc'])
+plt.plot(model.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+plt.plot(model.history['loss'])
+plt.plot(model.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+
+
+
+
+
+
+
